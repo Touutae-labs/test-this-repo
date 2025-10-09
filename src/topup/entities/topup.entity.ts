121fe8804cf37716c369860c0b602628ffee3d06
Touutae-labs <pantakan.totae@gmail.com>
2025-10-09 17:52:39 +0700

    Refactor Topup entity to include walletId and currency, and add TopupResponse class for enhanced transaction details

--- src/topup/entities/topup.entity.ts
+++ src/topup/entities/topup.entity.ts
@@ -1,23 +1,33 @@
-/**
- * Top-up Entity
- * Represents a top-up transaction
- */
+
 export enum TopupStatus {
   PENDING = 'PENDING',
   SUCCESS = 'SUCCESS',
   FAILED = 'FAILED',
 }
-
 export class Topup {
-  id: string;
-  userId: string;
+  referenceId: string;
+  walletId: string;
   amount: number;
-  status: TopupStatus;
-  externalTransactionId?: string;
+  currency: string;
+
+  constructor(partial: Partial<Topup>) {
+    Object.assign(this, partial);
+  }
+}
+
+export class TopupResponse {
+  requestId: string;
+  referenceId: string;
+  walletId: string;
+  amount: number;
+  currency: string;
+  status: TopupStatus
+  statusMessage?: string;
+  requestedAt: Date;
   createdAt: Date;
   updatedAt: Date;
 
-  constructor(partial: Partial<Topup>) {
+  constructor(partial: Partial<TopupResponse>) {
     Object.assign(this, partial);
   }
 }
